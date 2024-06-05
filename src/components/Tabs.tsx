183660a325a4de35305336679ac5a6e0762511da
Roneilla <bumanlar@sheridancollege.ca>
2024-06-05 13:04:16 -0400

    updated tab style and updated non-binary spelling

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -10,8 +10,8 @@ const Tabs = ({ currentCategory, changeCategory }: TabsProps) => {
 		<div className="w-full bg-white sm:pt-2">
 			<div className="flex items-end overflow-scroll px-4 gap-1">
 				<button
-					className={`font-medium tabItem text-black py-2 px-2 capitalize cursor-pointer hover:text-md hover:px-6 transition-all text-nowrap search ${
-						currentCategory === 'search' ? 'px-3 py-2.5' : ''
+					className={`font-medium tabItem text-black py-2 px-4 capitalize cursor-pointer hover:text-md hover:px-6 transition-all text-nowrap search ${
+						currentCategory === 'search' ? 'px-3 py-3' : ''
 					}`}
 					onClick={() => {
 						changeCategory('search');
@@ -23,19 +23,19 @@ const Tabs = ({ currentCategory, changeCategory }: TabsProps) => {
 						viewBox="0 0 20 20"
 						strokeWidth={1.5}
 						stroke="black"
-						className="size-4">
+						className="size-5">
 						<path
 							strokeLinecap="round"
 							strokeLinejoin="round"
 							d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
 						/>
 					</svg>
-				</button>{' '}
+				</button>
 				{clubCategories.sort().map((item) => (
 					<h2 key={item}>
 						<button
-							className={`font-medium tabItem text-black py-1 px-2 capitalize cursor-pointer hover:text-md hover:px-6 transition-all text-nowrap ${item} ${
-								item === currentCategory ? 'px-3 text-lg' : ''
+							className={`font-medium tabItem text-black py-2 px-4 capitalize cursor-pointer hover:text-md hover:px-6 transition-all text-nowrap ${item} ${
+								item === currentCategory ? 'text-lg' : ''
 							}`}
 							onClick={() => {
 								changeCategory(item);
